65903bf628819a05df436987286eea97c13cad49
RubenGlez <ruben.daren@gmail.com>
2023-01-27 16:14:08 +0100

    modifying  CI/CD

--- src/config/index.ts
+++ src/config/index.ts
@@ -2,6 +2,7 @@ import * as dotenv from 'dotenv'
 dotenv.config()
 
 // Environment variables
+export const ENVIRONMENT = String(process.env.ENVIRONMENT)
 export const DATABASE = String(process.env.DATABASE)
 export const PORT = Number(process.env.PORT)
 export const JWT_KEY = String(process.env.JWT_KEY)
