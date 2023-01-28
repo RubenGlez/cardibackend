480ff5733fe78122742012fb0f6bf1e4a24c78e6
RubenGlez <ruben.daren@gmail.com>
2023-01-28 18:52:42 +0100

    setting up the CI/CD

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,3 @@
-import express from 'express'
 import { DATABASE, PORT } from './config'
 import MongooseAdapter from './infrastructure/driven-adapters/mongoose'
 import Server from './infrastructure/driving-adapters/api-rest/server'
@@ -6,13 +5,9 @@ import Server from './infrastructure/driving-adapters/api-rest/server'
 const ServerInstance = new Server(PORT)
 const MongoInstance = new MongooseAdapter(DATABASE)
 
-const runApiRest = async (): Promise<express.Express> => {
+const runApiRest = async (): Promise<void> => {
   await ServerInstance.listen()
   await MongoInstance.connect()
-  const appInstance = ServerInstance.getAppInstance()
-  return appInstance
 }
 
-const apiInstance = runApiRest()
-
-module.exports = apiInstance
+void runApiRest()
